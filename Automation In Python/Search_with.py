bedac97cbd8adf1b0069ef56e6e96db3d43bbcdc
Master Ayush Gautam <ayushgautam2007.kg@gmail.com>
2026-04-14 14:15:14 +0530

    Add initial tkinter setup for social media search application and include a large PDF resource

---
+++ Automation In Python/Search_with.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+from tkinter import Enter, Label, Button 
+import webbrowser
+
+# define the main window 
+root = tk.Tk()
+root.title("Yours Social Media Paglu")
+
+# adding the background color
+root "entry" is not defined Pylance(reportUndefinedVariable)
