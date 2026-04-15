99a40d3e4c436de432d2fffbd1a567274c92b3cd
Master Ayush Gautam <ayushgautam2007.kg@gmail.com>
2026-04-15 22:50:28 +0530

    Refactor player choice implementation in reg lang.py

--- reg lang.py
+++ reg lang.py
@@ -1,3 +1,6 @@
-player_choice = "rock"
-if player_choice == "rock":
-    print("You chose rock!")
+import random
+def get_player_choices():
+    player_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+    while player_choice not in ["rock", "paper", "scissors"]:
+        print("Invalid choice. Please try again.")\
+            
